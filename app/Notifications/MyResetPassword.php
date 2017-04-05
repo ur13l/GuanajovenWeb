7cfea97f8188926c6f401d25ec9d901b02b8bc4b
Uriel <ur13l.infante@gmail.com>
2017-04-05 18:39:18 -0500

    Correo

---
+++ app/Notifications/MyResetPassword.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class MyResetPassword extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Recuperar contraseña Guanajoven')
+                    ->line('Hemos recibido una solicitud para reestablecer tu contraseña, para proceder con el cambio da ' .
+                    'click en el siguiente botón')
+                    ->action('Reestablecer contraseña', route('password.reset', $this->token))
+                    ->line('Agradecemos el uso de nuestra aplicación. ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
