a3dca2e7731467ca46683ca973373f6eaad7787d
Mario Alberto Negrete Rodríguez <mario_rguezz@yahoo.com.mx>
2017-06-27 12:49:29 -0700

    make it better

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -10,9 +10,28 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use App\CodigoGuanajoven;
+use App\Estado;
+use App\DatosUsuario;
+use App\Genero;
+//use App\Http\Controllers\Auth\ImageController;
+use App\Http\Controllers\ImageController;
+use App\Municipio;
+use App\Soap\ConsultaPorCurp;
+use App\Soap\ConsultaPorCurpResponse;
+use App\User;
+use Artisaninweb\SoapWrapper\SoapWrapper;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Validator;
 
 class ReportesController {
     public function index(Request $request) {
         return view('reportes.index');
     }
+
+
+
 }
