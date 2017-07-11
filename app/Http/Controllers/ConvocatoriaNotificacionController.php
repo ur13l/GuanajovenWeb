a1f451a8b91169ddea835b11e9a38dc4b85e5119
Mario Alberto Negrete Rodríguez <mario_rguezz@yahoo.com.mx>
2017-07-11 16:30:28 -0500

    cambio profile

--- app/Http/Controllers/ConvocatoriaNotificacionController.php
+++ app/Http/Controllers/ConvocatoriaNotificacionController.php
@@ -15,18 +15,19 @@ class ConvocatoriaNotificacionController extends Controller
     public function enviarNotificacion(Request $request) {
         $id_usuario = $request->input('id_usuario');
         $id_convocatoria = $request->input('id_convocatoria');
-
         if (NotificacionConvocatoria::where('id_usuario', $id_usuario)->where( 'id_convocatoria', $id_convocatoria)->count() > 0) {
             return response()->json(array(
                 "success" => false,
                 "status" => 500,
                 "errors" => []
             ));
+
         } else {
-            $usuario = User::where('id', '=', $request->input('id_usuario'))->firstOrFail();
+
+
+          $usuario   = User::where('id', '=', $request->input('id_usuario'))->firstOrFail();
             $convocatoria = Convocatoria::where('id_convocatoria', '=', $request->input('id_convocatoria'))->firstOrFail();
             $datos_usuario = DatosUsuario::where('id_usuario', '=', $request->input('id_usuario'))->firstOrFail();
-
             $usuario->notify(new ConvocatoriaNotification($convocatoria, $datos_usuario));
         }
     }
