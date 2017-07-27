c1311ec9848f97b6d5002bd4c0cdb906221881cc
Leonardo Lira Becerra <leonardolirabecerra@gmail.com>
2017-07-27 11:23:26 -0500

    Eventos

--- routes/api.php
+++ routes/api.php
@@ -53,6 +53,7 @@ Route::group(['prefix' => '/notificaciones'], function() {
     Route::post('/enviartoken', 'NotificacionesApiController@registrar');
     Route::post('/cancelartoken', 'NotificacionesApiController@cancelar');
     Route::post('/convocatoria', 'ConvocatoriaNotificacionController@enviarNotificacion');
+    Route::post('/evento', 'EventoNotificacionController@enviarNotificacion');
     Route::any('/convocatoria/registrada', 'EnviarCorreosApiController@index')->name('convocatoria.registrada');
     Route::any('/evento/registrado', 'EnviarCorreosApiController@guardarVariablesEvento')->name('evento.registrado');
 });
